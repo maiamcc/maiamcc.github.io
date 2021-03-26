c53a65512980e9c5c926f0b58544c2de4e77b5f1
Maia <maia.mcc@gmail.com>
2021-03-26 13:01:12 -0400

    Site updated at 2021-03-26 17:01:12 UTC

--- crosswords/crosswordify.sh
+++ crosswords/crosswordify.sh
@@ -6,15 +6,17 @@ set -ex
 #
 # Requires
 # - squares.io api key in env as SQUARES_IO_API_KEY
-# 	- docs: https://gist.github.com/dnr/4550a25c08a084965b755c5cfb0e9f26
+#   - see docs: https://gist.github.com/dnr/4550a25c08a084965b755c5cfb0e9f26
 # - kpuz script in path
 #   - https://github.com/kberg/kpuz/ and symlink xfer.py -> kpuz
 # - puz_to_pdf script in path
 #   - https://github.com/maiamcc/puz_to_pdf and symlink main.js -> puz_to_pdf
 
 IPUZ=$1
+
 # 1. make sure that the clues are in the correct order in the .ipuz file
 #    (first Across, then Down), or else squares.io shows them backwards
+#    (Crossfire exports them in the reverse order :-/)
 jq '.clues={"Across": .clues.Across, "Down": .clues.Down}' $IPUZ > tmp
 mv tmp $IPUZ
 
@@ -22,7 +24,7 @@ mv tmp $IPUZ
 PUZ="$(basename $IPUZ .ipuz).puz"
 kpuz $IPUZ $PUZ
 
-# # 3. create puzzle and solution pdfs
+# 3. create puzzle and solution pdfs
 puz_to_pdf $PUZ
 puz_to_pdf $PUZ --solution
 
